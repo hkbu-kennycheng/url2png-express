5d63e0763b86085c2b52601ff941bebf602e2d40
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2023-07-17 01:17:26 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # url2png-express
 A simple express server to return the URL screenshot captured by chromium
+
+## Dependencies
+
+This app assume [Chromium](https://www.chromium.org/getting-involved/download-chromium) in installed and available in the system path `/usr/bin/chromium`.
+
+## Installation
+
+```bash
+git clone https://github.com/hkbu-kennycheng/url2png-express.git
+cd url2png-express
+npm install
+npm start
+```
+
+## Usage
+
+Directly passing in the URL as the path parameter without the protocol prefix in `src` attribute of an ``<img>` tag. All URL is assumed to be HTTPS.
+
+```html
+<img src="http://localhost:3000/www.google.com" />
+```
+
+You could specify the viewport size by passing in the `width` and `height` path parameters.
+
+```html
+<img src="http://localhost:3000/<width>/<height>/www.google.com" />
+```
+
+You could also specify the viewport size by passing in the `width` path parameter only. The height will be calculated based on 16:9 aspect ratio of the viewport.
+
+```html
+<img src="http://localhost:3000/<width>/www.google.com" />
+```
---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const puppeteer = require('puppeteer-core')
+const fs = require('fs');
+const path = require('path');
+const { mkdirp } = require('mkdirp');
+
+app.get("*", async (req, res) => {
+    let url = req.path.substring(1);
+    let parts = url.split('/');
+    let width = 1280;
+    let height = 720;
+
+    const dir = path.dirname(`/tmp/${url}`);
+    const filename = `/tmp/${url}.png`;
+    if (dir) {
+        mkdirp(dir);
+    }
+
+    if (fs.existsSync(filename)) {
+        return res.sendFile(filename);
+    }
+
+    if (parts[0] && parseInt(parts[0])) {
+        width = parseInt(parts[0]);
+        parts.splice(0, 1);
+        url = parts.join('/');
+        if (parts[0] && parseInt(parts[0])) {
+            height = parseInt(parts[0]);
+            parts.splice(0, 1);
+            url = parts.join('/');
+        } else {
+            height = Math.ceil(width / 16 * 9);
+        }
+    }
+
+    try {
+        const browser = await puppeteer.launch({
+            executablePath:'/usr/bin/chromium', headless:'new', args:['--no-sandbox']
+        });
+
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: width,
+            height: height,
+            deviceScale: 1
+        });
+        await page.goto(`https://${url}`, {waitUntil:'networkidle0'});
+        await page.screenshot({path: filename});
+        await browser.close();
+
+        return res.sendFile(filename);
+    } catch(ex) {
+        console.log(ex);
+        return res.sendStatus(500);
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Listen on the port 3000...");
+});
